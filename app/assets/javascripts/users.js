95437231fdc4aca3e5028afcf53328c2f459bed0
Kasey O'Neill <kasey.e.oneill@gmail.com>
2017-03-22 19:54:38 +0000

    Added Stripe js - sending card fields, response handler and error handling js.

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,51 @@
+/* global $, Stripe */
+$(document).on('turbolinks:load', function(){
+ var theForm = $('#pro-form');
+ var submitBtn = $('#form-submit-btn');
+ 
+ Stripe.setPublishableKey($('meta[name="stripe-key]').attr('content'));
+ 
+ submitBtn.click(function(event){
+   event.preventDefault();
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+        
+    
+    //Use Stripe JS library to check for card errors
+    var error = false;
+    
+    if(!Stripe.card.validateCardNumber(ccNum)){
+      error = true;
+      alert('The credit card number appears to be invalid');
+    }
+    
+    if(!Stripe.card.validateCVC(cvcNum)){
+      error = true;
+      alert('The CVC number appears to be invalid');
+    }
+    
+    if(!Stripe.card.validateExpiry(expMonth, expYear)){
+      error = true;
+      alert('The expiration date appears to be invalid');
+    }
+    
+    if (error){
+      submitBtn.prop('disabled', false).val("Sign Up");
+    } else {
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    return false;
+ });
+ function stripeResponseHandler(status, response){
+   var token = response.id;
+   theForm.append($('<input type="hidden" name="user[stripe_card_token]">').val(token));
+   theForm.get(0).submit();
+ }
+});
